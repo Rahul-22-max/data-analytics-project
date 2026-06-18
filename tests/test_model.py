01ece279f4bc13ecb4e8817c498e2a1894a03ec0
mohamedjaveethedu-hub <mohamedjaveethedu@gmail.com>
2026-06-18 15:55:27 +0530

    DAY 5: Tested customer model and verified table creation

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,6 +1,10 @@
 from src.models.customer import Customer
 
 def test_customer_model():
-    customer = Customer()
+    customer = Customer(
+        customerID="0001",
+        gender="Male"
+    )
 
-    assert customer is not None
+    assert customer.customerID == "0001"
+    assert customer.gender == "Male"
