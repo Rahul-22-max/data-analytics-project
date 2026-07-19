6bdd772bc3001e9a8bea4de5868b36a73d78e32c
Rahul-22-max <rahulcharanboola05@gmail.com>
2026-07-19 17:33:27 +0530

    member5 day12: added preprocessing for ML model

--- app/services/prediction_service.py
+++ app/services/prediction_service.py
@@ -1,11 +1,16 @@
 from app.schemas.prediction import PredictionRequest, PredictionResponse
 from app.services.model_loader import model
+from app.services.preprocessing import preprocess_input
 
 
 class PredictionService:
 
     @staticmethod
     def predict(data: PredictionRequest):
+
+        features = preprocess_input(data)
+
+        print(features)
 
         if model is None:
             print("⚠ Using fallback prediction.")
@@ -15,8 +20,12 @@ class PredictionService:
                 probability=0.91
             )
 
-        # Real prediction will be added later
+        # This will be used when the real model is added
+        prediction = model.predict(features)[0]
+
+        probability = model.predict_proba(features)[0][1]
+
         return PredictionResponse(
-            churn_prediction="Yes",
-            probability=0.97
+            churn_prediction="Yes" if prediction == 1 else "No",
+            probability=round(float(probability), 2)
         )
