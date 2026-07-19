83fd711aed6e3d82e7d418e98fa6d6e7ed9148dc
Rahul-22-max <rahulcharanboola05@gmail.com>
2026-07-19 16:45:50 +0530

    member5 day5: add logging and global exception handling

--- app/routes/prediction.py
+++ app/routes/prediction.py
@@ -1,11 +1,19 @@
 from fastapi import APIRouter
 from app.schemas.prediction import PredictionRequest, PredictionResponse
+from app.utils.logger import logger
 
-router = APIRouter(prefix="/predict", tags=["Prediction"])
+router = APIRouter(
+    prefix="/predict",
+    tags=["Prediction"]
+)
 
 
 @router.post("/", response_model=PredictionResponse)
 def predict(request: PredictionRequest):
+
+    logger.info("Prediction API called")
+
+    logger.info(f"Request Data: {request}")
 
     return PredictionResponse(
         churn_prediction="No",
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 
 from app.core.config import settings
+from app.core.exceptions import global_exception_handler
+
 from app.routes.home import router as home_router
 from app.routes.prediction import router as prediction_router
 
@@ -9,6 +11,8 @@ app = FastAPI(
     description=settings.APP_DESCRIPTION,
     version=settings.APP_VERSION,
 )
+
+app.add_exception_handler(Exception, global_exception_handler)
 
 app.include_router(home_router)
 app.include_router(prediction_router)
