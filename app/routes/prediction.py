0944748d0350f9248e2185fb166dd240c1894fa6
Rahul-22-max <rahulcharanboola05@gmail.com>
2026-07-19 19:48:44 +0530

    member5 day19: added dependency injection using Depends

--- app/routes/prediction.py
+++ app/routes/prediction.py
@@ -1,7 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
-from app.schemas.prediction import PredictionRequest, PredictionResponse
+from app.schemas.prediction import (
+    PredictionRequest,
+    PredictionResponse,
+)
 from app.services.prediction_service import PredictionService
+from app.dependencies.prediction_dependency import (
+    get_prediction_service,
+)
 from app.utils.logger import logger
 
 router = APIRouter(
@@ -11,12 +17,15 @@ router = APIRouter(
 
 
 @router.post("/", response_model=PredictionResponse)
-def predict(request: PredictionRequest):
+def predict(
+    request: PredictionRequest,
+    service: PredictionService = Depends(get_prediction_service)
+):
 
     logger.info("Prediction API called")
     logger.info(f"Request Data: {request}")
 
-    response = PredictionService.predict(request)
+    response = service.predict(request)
 
     logger.info(f"Prediction Response: {response}")
 
