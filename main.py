565f4d75cee8267c7a91acdb1523979683faeaf3
Rahul-22-max <rahulcharanboola05@gmail.com>
2026-07-19 20:28:16 +0530

    member5 day25: added GZip response compression

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.gzip import GZipMiddleware
 
 from app.core.config import settings
 from app.core.exceptions import APIException, api_exception_handler
@@ -33,17 +35,16 @@ app = FastAPI(
     description="""
 ## Customer Churn Prediction API
 
-This API predicts whether a customer is likely to churn.
+This API predicts customer churn.
 
 ### Features
 
 - Customer Churn Prediction
 - Health Check
-- Standard API Responses
-- Request Logging
-- Environment Configuration
-
-Built with FastAPI.
+- Logging
+- Environment Variables
+- CORS
+- GZip Compression
 """,
     version=settings.APP_VERSION,
     terms_of_service="https://example.com/terms",
@@ -57,13 +58,38 @@ Built with FastAPI.
     lifespan=lifespan,
 )
 
+# -------------------------
 # Middleware
+# -------------------------
+
 app.add_middleware(RequestLoggerMiddleware)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.add_middleware(
+    GZipMiddleware,
+    minimum_size=1000
+)
+
+# -------------------------
 # Exception Handler
+# -------------------------
+
 app.add_exception_handler(APIException, api_exception_handler)
 
+# -------------------------
 # Routers
+# -------------------------
+
 app.include_router(home_router, prefix=settings.API_PREFIX)
 app.include_router(health_router, prefix=settings.API_PREFIX)
 app.include_router(prediction_router, prefix=settings.API_PREFIX)
